b38772d5900385a86bfd535591905d990a0561df
wenqian1234 <72588646+wenqian1234@users.noreply.github.com>
2021-02-26 19:52:14 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,12 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:wenqian1234/wenqian1234.github.io.git master
+# git push -f git@github.com:wenqian1234/wenqian1234.github.io.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
+git push -f git@github.com:wenqian1234/docs.git master:gh-pages
+
 
 cd -
 
