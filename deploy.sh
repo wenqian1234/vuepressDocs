6414d7c78bda5a8044513d0b63d9d945e4d475ad
wenqian1234 <72588646+wenqian1234@users.noreply.github.com>
2021-02-26 17:16:22 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,26 @@
 #!/usr/bin/env sh
+
 # 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+# echo 'www.yourwebsite.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# 如果你想要部署到 https://USERNAME.github.io
+git push -f git@github.com:wenqian1234/wenqian1234.github.io.git master
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-# 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
-git push -f git@github.com:wenqian1234/vuepressDocs.git master:gh-pages
+# 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
+# git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
 
 cd -
 
