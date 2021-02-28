fa1b8c985e243a46bdc828da1c329f30426509a5
wenqian1234 <72588646+wenqian1234@users.noreply.github.com>
2021-02-28 16:53:41 +0800

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title:'文档管理',
     description:'Hello VuePress',
-    base:'https://wenqian1234.github.io/vuepressdocs/index.html',
+    base:'wenqian1234.github.com',
     head:[
         ['link',{rel:'icon',href:'/favicon.ico'}]
     ],
