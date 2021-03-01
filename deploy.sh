2559793290fd28a439e0a2f49aba52e8c768a606
wenqian1234 <72588646+wenqian1234@users.noreply.github.com>
2021-03-01 09:31:28 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd .vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.yourwebsite.com' > CNAME
@@ -17,12 +17,11 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:wenqian1234/wenqian1234.github.io.git master
+# git push -f git@github.com:wenqian1234/wenqian1234.github.io.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
-git push -f git@github.com:wenqian1234/vuepressDocs.git master:gh-pages
-
+git push -f git@github.com:wenqian1234/vuepressdocs.git master:test
 
 cd -
-
+# 最后发布的时候执行 bash deploy.sh
